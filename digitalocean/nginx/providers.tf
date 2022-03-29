1e386554427507a702695278a3488b859d2a98d0
jkleinkauff <jhonatas.kleinkauff@loadsmart.com>
2022-03-28 22:02:05 -0300

    tf fmt

--- digitalocean/nginx/providers.tf
+++ digitalocean/nginx/providers.tf
@@ -1,7 +1,7 @@
 provider "helm" {
   kubernetes {
-    host             = var.k8s_endpoint
-    token            = var.k8s_token
+    host                   = var.k8s_endpoint
+    token                  = var.k8s_token
     cluster_ca_certificate = var.k8s_certificate
   }
 }
